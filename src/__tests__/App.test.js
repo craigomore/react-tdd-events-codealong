7feb31296bb1af16e1f9fb58660da095696638b8
Craig Omore <craiggomore@gmail.com>
2025-10-27 22:26:34 +0300

    finish

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,10 @@
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
 import App from "../App";
 
-import '@testing-library/jest-dom';
-
-// Code tests here
+test("renders without crashing", () => {
+  render(<App />);
+  const heading = screen.getByText(/select pizza toppings/i);
+  expect(heading).toBeInTheDocument();
+});
